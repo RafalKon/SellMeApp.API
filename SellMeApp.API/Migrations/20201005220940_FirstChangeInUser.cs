666bb209807914f67ef52b8e08902a561710136c
DESKTOP-L19MTDP\Rafał <Rafal.Kon1995@gmail.com>
2020-10-06 00:10:29 +0200

    First change in UserModel.

---
+++ SellMeApp.API/Migrations/20201005220940_FirstChangeInUser.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SellMeApp.API.Migrations
+{
+    public partial class FirstChangeInUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordHash",
+                table: "UsersModel",
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordSalt",
+                table: "UsersModel",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Username",
+                table: "UsersModel",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "UsersModel");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordSalt",
+                table: "UsersModel");
+
+            migrationBuilder.DropColumn(
+                name: "Username",
+                table: "UsersModel");
+        }
+    }
+}
